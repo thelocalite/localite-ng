b3bd2bc02a1fc6b2a250d5a3e31cbb973cde2046
Vijaya Aditya <vatadepalli@hotmail.com>
2020-01-16 01:54:26 +0530

    hate that search bar

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as M from '../../../assets/js/materialize.min';
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('select');
+      var instances = M.FormSelect.init(elems, {});
+    });
   }
-
 }

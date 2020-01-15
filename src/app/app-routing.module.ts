aebe73da003fdb3bd2296e40cb22594ebc9ba089
Vijaya Aditya <vatadepalli@hotmail.com>
2020-01-15 19:15:37 +0530

    cart and location components and menubar icosn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { HomeComponent } from "./layout/home/home.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { RegisterComponent } from "./auth/register/register.component";
-import { LogoutComponent } from "./auth/logout/logout.component";
+import {LoginComponent} from './auth/login/login.component';
+import {LogoutComponent} from './auth/logout/logout.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {HomeComponent} from './layout/home/home.component';
+import {CartComponent} from './user/cart/cart.component';
+import {LocationComponent} from './user/location/location.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "login", component: LoginComponent },
-  { path: "register", component: RegisterComponent },
-  { path: "logout", component: LogoutComponent }
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'logout', component: LogoutComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'location', component: LocationComponent},
 ];
 
-@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
-export class AppRoutingModule {}
+@NgModule({imports: [RouterModule.forRoot(routes)], exports: [RouterModule]})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { HomeComponent } from './layout/home/home.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ChangePasswordComponent } from './auth/change-password/change-password.component';
-import { UsersComponent } from './auth/users/users.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { LogoutComponent } from './auth/logout/logout.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ChangePasswordComponent} from './auth/change-password/change-password.component';
+import {LoginComponent} from './auth/login/login.component';
+import {LogoutComponent} from './auth/logout/logout.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {UsersComponent} from './auth/users/users.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {HomeComponent} from './layout/home/home.component';
+import {NavbarComponent} from './layout/navbar/navbar.component';
+import {CartComponent} from './user/cart/cart.component';
+import {LocationComponent} from './user/location/location.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    ChangePasswordComponent,
-    UsersComponent,
-    FooterComponent,
-    LogoutComponent
+    AppComponent, NavbarComponent, HomeComponent, LoginComponent,
+    RegisterComponent, ChangePasswordComponent, UsersComponent, FooterComponent,
+    LogoutComponent, CartComponent, LocationComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

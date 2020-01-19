baeab361ab22ed81fe494f8424f218c124a02394
Vijaya Aditya <vatadepalli@hotmail.com>
2020-01-20 03:57:54 +0530

    using radios instead of dropdown

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -40,11 +40,7 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    document.addEventListener("DOMContentLoaded", function() {
-      var elems = document.querySelectorAll("select");
-      var options = document.querySelectorAll("option");
-      var instances = M.FormSelect.init(elems, options);
-    });
+    document.addEventListener("DOMContentLoaded", function() {});
   }
 
   login() {

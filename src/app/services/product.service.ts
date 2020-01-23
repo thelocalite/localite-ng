308c5b2ac601bd2496f85d34c52f6d3b4b682f2e
technologic <20627603+technologic808@users.noreply.github.com>
2020-01-23 19:17:05 +0530

    Surprise team members through change of Port

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,7 +7,7 @@ import { map, tap, catchError } from "rxjs/operators";
 @Injectable({ providedIn: "root" })
 export class ProductService {
   // private productsUrl = "https://my-json-server.typicode.com/technologic808/json-server/product"; // URL to web api
-  private productsUrl = "http://localhost:8080/product"; // URL to web api
+  private productsUrl = "http://localhost/product"; // URL to web api
 
   httpOptions = {
     headers: new HttpHeaders({ "Content-Type": "application/json" })

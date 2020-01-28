630ed3e7568df166dc355853efd74f315ecaa153
technologic <20627603+technologic808@users.noreply.github.com>
2020-01-28 17:42:31 +0530

    Implement Add Store Component
    Form and functionality

--- src/app/components/data/add-store/add-store.component.ts
+++ src/app/components/data/add-store/add-store.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
+import { Store } from 'src/app/models/store';
 
 @Component({
   selector: 'app-add-store',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-store.component.css']
 })
 export class AddStoreComponent implements OnInit {
+  // Add Store to bind to form
+  store: Store = new Store();
 
-  constructor() { }
+  constructor(private storeService: StoreService) { }
 
   ngOnInit() {
   }
 
+  /**
+   * @method addStore :  Adds Store to Database 
+   * Calls addStore() method of Store Service 
+   * to send Store to Spring Boot REST API 
+   * using POST method 
+   */
+
+  addStore() {
+    console.log(this.store);
+    this.storeService.addStore(this.store).subscribe({
+      // completeHandler
+      complete: () => {
+        console.log("Store add completed");
+        this.resetForm();
+      },
+    });
+  }
+
+  // Resets form by resetting product object
+  resetForm() {
+    this.store = new Store();
+  }
 }

3c77cef7bfc7cb1db4058f741b97646b0f3bea2f
ndivya03 <divyasundaram95@gmail.com>
2020-01-29 19:00:39 +0530

    Update Cart model.

--- src/app/models/cartProducts.ts
+++ src/app/models/cartProducts.ts
@@ -1,7 +1,9 @@
 import { Product } from './product';
+import { Store } from './store';
 
 export interface CartProduct {
   id: number;
+  vendor: Store;
   product:  Product;
   quantity: number;
   price: number;

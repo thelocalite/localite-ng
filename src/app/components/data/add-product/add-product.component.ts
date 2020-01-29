2c4260cf8d9fdc0c154719a835f271ada4b3f585
technologic <20627603+technologic808@users.noreply.github.com>
2020-01-29 17:52:32 +0530

    Integrate Image Uploader and Finish Add Product Page
    Image Uploader is now an independent component with an @Output field which is a two way binding and can be bound to any Property

--- src/app/components/data/add-product/add-product.component.ts
+++ src/app/components/data/add-product/add-product.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Product } from 'src/app/models/product';
-import { ProductService } from 'src/app/services/product.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { Product } from "src/app/models/product";
+import { ProductService } from "src/app/services/product.service";
 
 @Component({
-  selector: 'app-add-product',
-  templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  selector: "app-add-product",
+  templateUrl: "./add-product.component.html",
+  styleUrls: ["./add-product.component.css"]
 })
 export class AddProductComponent implements OnInit {
   // Add Product to bind to form
   product: Product = new Product();
 
-  constructor(private productService: ProductService) { }
-
-  ngOnInit() {
+  ngOnChanges($event) {
+    console.log("event : " + event);
   }
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {}
+
   /**
-   * @method addProduct :  Adds Product to Database 
-   * Calls addProduct() method of Product Service 
-   * to send Product to Spring Boot REST API 
-   * using POST method 
+   * @method addProduct :  Adds Product to Database
+   * Calls addProduct() method of Product Service
+   * to send Product to Spring Boot REST API
+   * using POST method
    */
   addProduct() {
     console.log(this.product);
@@ -30,12 +33,13 @@ export class AddProductComponent implements OnInit {
       complete: () => {
         console.log("Product add completed");
         this.resetForm();
-      },
+      }
     });
   }
 
   // Resets form by resetting product object
   resetForm() {
     this.product = new Product();
+    this.product.imageUrl = "";
   }
 }

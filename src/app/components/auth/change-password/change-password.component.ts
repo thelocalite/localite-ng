c0a798c6870fbd6faef019877910e67c29a48389
10maycdac <55127938+10maycdac@users.noreply.github.com>
2020-01-29 23:01:33 +0530

    Improve all the forms and add change-password template

--- src/app/components/auth/change-password/change-password.component.ts
+++ src/app/components/auth/change-password/change-password.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-change-password',
-  templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.css']
+  selector: "app-change-password",
+  templateUrl: "./change-password.component.html",
+  styleUrls: ["./change-password.component.css"]
 })
 export class ChangePasswordComponent implements OnInit {
+  oldPassword: string;
+  newPassword: string;
+  cNewPassword: string;
+  failedToChange = null;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  changePassword() {}
 }
--- src/app/components/auth/forgot-password/forgot-password.component.ts
+++ src/app/components/auth/forgot-password/forgot-password.component.ts
@@ -1,39 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from 'src/app/services/auth/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  selector: "app-forgot-password",
+  templateUrl: "./forgot-password.component.html",
+  styleUrls: ["./forgot-password.component.css"]
 })
 export class ForgotPasswordComponent implements OnInit {
   email = false;
   nomail = false;
 
-  forgotEmail = '';
+  forgotEmail = "";
 
   token = null;
 
-  password = '';
-  confirmPassword = '';
+  password = "";
+  confirmPassword = "";
   notMatching = false;
 
-
   changedPassword = false;
-  updatedPassword = false;
+  updatedPassword = null;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.token = window.location.pathname.split('/')[2];
-    console.log('***************' + this.token);
+    this.token = window.location.pathname.split("/")[2];
+    console.log("***************" + this.token);
   }
 
   forgotPassword() {
     this.email = false;
     this.nomail = false;
-    if (this.forgotEmail != '') {
-      this.authService.forgotPassword(this.forgotEmail).subscribe((data) => {
+    if (this.forgotEmail != "") {
+      this.authService.forgotPassword(this.forgotEmail).subscribe(data => {
         if (data === true) {
           this.email = true;
           this.nomail = false;
@@ -48,18 +47,20 @@ export class ForgotPasswordComponent implements OnInit {
   updatePassword() {
     this.notMatching = false;
 
-    if (this.token != '' && this.password != '') {
+    if (this.token != "" && this.password != "") {
       if (this.password === this.confirmPassword) {
         // console.log(this.token);
         // console.log(this.password);
-        this.authService.updatePassword(this.token, this.password)
-            .subscribe(data => {
-              console.log(data);
-              if (data.msg === 'success') {
-                this.updatedPassword = true;
-              }
-            });
+        this.authService
+          .updatePassword(this.token, this.password)
+          .subscribe(data => {
+            console.log(data);
+            if (data.msg === "success") {
+              this.updatedPassword = true;
+            }
+          });
       } else {
+        this.updatedPassword = false;
         this.notMatching = true;
       }
     }

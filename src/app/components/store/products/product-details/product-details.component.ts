93a39efefbc61bc9ae37127b29ad0f46ddf274f6
technologic <20627603+technologic808@users.noreply.github.com>
2020-01-31 00:12:04 +0530

    Change Product Card based on Context

--- src/app/components/store/products/product-details/product-details.component.ts
+++ src/app/components/store/products/product-details/product-details.component.ts
@@ -1,40 +1,58 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
-import { Product } from 'src/app/models/product';
-import { ProductService } from 'src/app/services/product.service';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Product } from "src/app/models/product";
+import { ProductService } from "src/app/services/product.service";
 
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 /**
  * Product Details Component
- * 
+ *
  * This is the Product Card
- * 
+ *
  * This card displays product information which can be reused by other components
- * 
+ *
  */
 export class ProductDetailsComponent implements OnInit {
-
   // Takes product id as input from parent component
   @Input() productId: number;
   product: Product;
 
+  // Flag to check if page is Product Page
+  isProductPage: boolean = false;
+
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
-  ) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    // Checks URL
+    this.checkPageUrl();
+
+    // Gets Product
     this.getProduct();
   }
 
+  // Gets Product By Product ID
   getProduct(): void {
-    this.productService.getProduct(this.productId).subscribe(product => (this.product = product));
+    this.productService
+      .getProduct(this.productId)
+      .subscribe(product => (this.product = product));
   }
 
-
+  // Checks if the current page is the product page and updates the isProductPageFlag
+  checkPageUrl() {
+    this.router.url
+      .split("/")
+      .forEach(route =>
+        route == "product"
+          ? (this.isProductPage = true)
+          : (this.isProductPage = false)
+      );
+  }
 }

ca8b5e55931bafb295f0cb37d506645af5bbb470
technologic <20627603+technologic808@users.noreply.github.com>
2020-02-01 01:02:58 +0530

    Overhaul Store Page and Product Page :relaxed:
    Routes established for an interconnected path of user interaction

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,11 +1,12 @@
 // Defines Product Model for rendering
 
 export class Product {
-    id: number;
-    name: string;
-    brand: string;
-    imageUrl: string;
-    description: string;
-    mrp: number;
-    category: string;
+  id: number;
+  name: string;
+  brand: string;
+  imageUrl: string;
+  description: string;
+  mrp: number;
+  category: string;
+  discountedPrice: number;
 }
